619c5200a7968944da1fa8be220bb0964eb47693
Devone Austin <devone.austin@gmail.com>
2020-04-24 16:13:43 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,7 +45,7 @@ class TicTacToe
   end
 
   def valid_move?(index)
-    if !position_taken(index) && index.between[0, 8]
+    if !position_taken?(index) && index.between[0, 8]
       true
     else
       false
